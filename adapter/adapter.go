038eb7ab16bb233b3f0eca23d434d40ee1505be5
Thobian <suixinsuoyu1hao@gmail.com>
2026-03-01 17:40:59 +0800

    删除冗余代码

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -273,7 +273,7 @@ func (p *Proxy) URLTest(ctx context.Context, url string, expectedStatus utils.In
 		}
 	}
 
-	satisfied = resp != nil && (expectedStatus == nil || expectedStatus.Check(uint16(resp.StatusCode)))
+	satisfied = expectedStatus == nil || expectedStatus.Check(uint16(resp.StatusCode))
 	if !satisfied {
 		log.Errorln("%s resp.StatusCode is %d, not expected status %v", p.Name(), resp.StatusCode, expectedStatus)
 		return
